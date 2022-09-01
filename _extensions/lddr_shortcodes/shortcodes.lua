c2cda2a143732c6931eac225e6fb5fedbcc90b1d
Pascal Burkhard <pascal.burkhard@gmail.com>
2022-09-01 15:55:11 +0200

    Add shortcode for pdf download link

---
+++ _extensions/lddr_shortcodes/shortcodes.lua
@@ -0,0 +1,27 @@
+return {
+  ["doc-source"] = function(args, kwargs, meta)
+    local calloutDiv = pandoc.Div({})
+    calloutDiv.attr.classes:insert("callout-note")
+    calloutDiv.content:insert(pandoc.Header(2, "Test"))
+    calloutDiv.content:insert("Abc")
+    
+    -- return calloutDiv
+    return nil
+  end,
+  
+  ["download-pdf"] = function()
+    if quarto.doc.isFormat("html") then
+      local fileLink = PANDOC_STATE.output_file:gsub(".html","") .. ".pdf"
+      
+      local downloadLink = pandoc.Link("", fileLink)
+      downloadLink.content:insert("Télécharger au format")
+      downloadLink.content:insert(pandoc.RawInline('html', '<i class="fa-solid fa-file-pdf" aria-hidden="true"></i>'))
+      
+      local asideSpan = pandoc.Span({})
+      asideSpan.attr.classes:insert("aside")
+      asideSpan.content:insert(downloadLink)
+        
+      return downloadLink
+    end
+  end
+}
