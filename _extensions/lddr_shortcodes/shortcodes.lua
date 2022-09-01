16b12595da3d740e8b6314724ba74852837486f4
Pascal Burkhard <pascal.burkhard@gmail.com>
2022-09-01 16:13:14 +0200

    Add fontawesome dependency and return Span instead of link

--- _extensions/lddr_shortcodes/shortcodes.lua
+++ _extensions/lddr_shortcodes/shortcodes.lua
@@ -11,17 +11,23 @@ return {
   
   ["download-pdf"] = function()
     if quarto.doc.isFormat("html") then
+      quarto.doc.addHtmlDependency({
+        name = 'fontawesome6',
+        version = '0.1.0',
+        stylesheets = {'assets/css/all.css'}
+      })
+      
       local fileLink = PANDOC_STATE.output_file:gsub(".html","") .. ".pdf"
       
       local downloadLink = pandoc.Link("", fileLink)
-      downloadLink.content:insert("Télécharger au format")
+      downloadLink.content:insert("Télécharger au format ")
       downloadLink.content:insert(pandoc.RawInline('html', '<i class="fa-solid fa-file-pdf" aria-hidden="true"></i>'))
       
       local asideSpan = pandoc.Span({})
       asideSpan.attr.classes:insert("aside")
       asideSpan.content:insert(downloadLink)
         
-      return downloadLink
+      return asideSpan
     end
   end
 }
