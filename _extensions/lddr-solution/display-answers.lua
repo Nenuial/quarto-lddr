44b3b83e4a1706ef84ee49c669ebf78a5d189298
Pascal Burkhard <pascal.burkhard@gmail.com>
2022-09-15 15:49:33 +0200

    Switch from underscore to dash and add solution filter

---
+++ _extensions/lddr-solution/display-answers.lua
@@ -0,0 +1,55 @@
+local solutionStatus = false
+local questionNumber = 0
+
+local function readStatus(meta)
+  if meta['solutions'] then
+    solutionStatus = true
+    
+    if quarto.doc.isFormat("latex") then
+      meta['classoption']:insert(pandoc.Str('answers'))
+    end
+    
+    return meta
+  end
+end
+
+local function writeSolutions(divEl)
+  if divEl.attr.classes:includes('answer') then
+    if quarto.doc.isFormat("html") and not solutionStatus then
+      return {}
+    end
+    
+    if solutionStatus then
+      divEl.attr.classes:insert('callout-note')
+      
+      divEl.content:insert(1, pandoc.Header(2, "Solution"))
+      
+      return divEl
+    end
+  end
+  
+  return divEl
+end
+
+local function writeQuestions(paraEl)
+  if quarto.doc.isFormat("html") then
+    firstEl = paraEl.content[1]
+    
+    if firstEl.attr == nil then
+      return paraEl
+    end
+  
+    if firstEl.attr.classes:includes('q') then
+      questionNumber = questionNumber + 1
+      return pandoc.OrderedList(paraEl, pandoc.ListAttributes(questionNumber))
+    end
+  end
+  
+  return paraEl
+end
+
+return {
+  {Meta = readStatus},
+  {Div = writeSolutions},
+  {Para = writeQuestions}
+}
