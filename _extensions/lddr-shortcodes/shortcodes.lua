8e622712647a2ce7701ba2bedfdf23e4c6b6ad90
Pascal Burkhard <pascal.burkhard@gmail.com>
2023-01-31 06:46:58 +0100

    Update shortcodes

--- _extensions/lddr-shortcodes/shortcodes.lua
+++ _extensions/lddr-shortcodes/shortcodes.lua
@@ -1,41 +1,23 @@
 return {
   ["doc-source"] = function(args, kwargs, meta)
     if quarto.doc.isFormat("pdf") then
-      local authorName = pandoc.utils.stringify(meta["authors"][1]["name"]["literal"])
+      local authorName = pandoc.utils.stringify(meta["authors"][1]["name"])
       local sourceDate = pandoc.utils.stringify(meta["date"])
-      local sourceName = pandoc.utils.stringify(meta["affiliations"][1]["name"])
-      local sourceUrl = pandoc.utils.stringify(meta["affiliations"][1]["url"])
+      local sourceName = pandoc.utils.stringify(meta["authors"][1]["affiliation"]["name"])
+      local sourceUrl = pandoc.utils.stringify(meta["authors"][1]["affiliation"]["url"])
       
-      local calloutDiv = pandoc.Div({})
-      calloutDiv.attr.classes:insert("callout-note")
-      calloutDiv.attr.attributes["icon"] = "false"
-      calloutDiv.content:insert(pandoc.Header(2, "Article de presse"))
-      calloutDiv.content:insert(pandoc.Inlines{"Cet article de ", pandoc.Emph(authorName), " a été publié le ", sourceDate, " par ", pandoc.Emph(pandoc.Link(sourceName, sourceUrl)), "."})
+      local calloutHeader = pandoc.Header(2, "Article de presse")
+      local calloutContent = pandoc.Inlines{"Cet article de ", pandoc.Emph(authorName), 
+        " a été publié le ", sourceDate, " par ", 
+        pandoc.Emph(pandoc.Link(sourceName, sourceUrl)), "."}
       
-      -- return calloutDiv
-      return calloutDiv
-    end
-  end,
-  
-  ["download-pdf"] = function()
-    if quarto.doc.isFormat("html") then
-      quarto.doc.addHtmlDependency({
-        name = 'fontawesome6',
-        version = '0.1.0',
-        stylesheets = {'assets/css/all.css'}
-      })
+      local calloutDiv = {}
+      calloutDiv["type"] = "note"
+      calloutDiv["content"] = pandoc.Blocks{calloutHeader, calloutContent}
       
-      local fileLink = PANDOC_STATE.output_file:gsub(".html","") .. ".pdf"
+      calloutOut = quarto.Callout(calloutDiv)
       
-      local downloadLink = pandoc.Link("", fileLink)
-      downloadLink.content:insert("Télécharger au format ")
-      downloadLink.content:insert(pandoc.RawInline('html', '<i class="fa-solid fa-file-pdf" aria-hidden="true"></i>'))
-      
-      local asideSpan = pandoc.Span({})
-      asideSpan.attr.classes:insert("aside")
-      asideSpan.content:insert(downloadLink)
-        
-      return asideSpan
+      return calloutOut
     end
   end
 }
